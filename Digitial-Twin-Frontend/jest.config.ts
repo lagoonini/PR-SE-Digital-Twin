64ee15ff72e223b61565464dc8ce46bcefed213c
lagoonini <zaprojekat@proton.me>
2024-04-23 21:43:46 +0200

    Commit

--- Digitial-Twin-Frontend/jest.config.ts
+++ Digitial-Twin-Frontend/jest.config.ts
@@ -1,20 +1,26 @@
-module.exports = {
+import type { Config } from 'jest';
+
+const config: Config = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
-    testPathIgnorePatterns: [
-        "<rootDir>/node_modules/",
-        "<rootDir>/dist/"
-    ],
     globals: {
         'ts-jest': {
             tsconfig: '<rootDir>/tsconfig.spec.json',
             stringifyContentPathRegex: '\\.html$',
-            diagnostics: false
+            diagnostics: {
+                warnOnly: true
+            }
         }
     },
     moduleNameMapper: {
         '@app/(.*)': '<rootDir>/src/app/$1',
         '@assets/(.*)': '<rootDir>/src/assets/$1',
-        '@env': '<rootDir>/src/environments/environment',
-    }
+        '@env': '<rootDir>/src/environments/environment'
+    },
+    transform: {
+        '^.+\\.(ts|html)$': 'ts-jest'
+    },
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/']
 };
+
+export default config;
--- Digitial-Twin-Frontend/src/app/room/room-item/room-item.component.spec.ts
+++ Digitial-Twin-Frontend/src/app/room/room-item/room-item.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { RoomItemComponent } from './room-item.component';
 
 describe('RoomItemComponent', () => {
@@ -7,7 +8,7 @@ describe('RoomItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [RoomItemComponent]
+      imports: [RoomItemComponent]
     })
         .compileComponents();
 
