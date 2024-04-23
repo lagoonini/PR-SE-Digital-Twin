315fbde65ec01ba7d1766913101f4c1ec440a92d
lagoonini <zaprojekat@proton.me>
2024-04-24 00:20:36 +0200

    Commit

--- Digitial-Twin-Frontend/src/app/room/room-item/room-item.component.spec.ts
+++ Digitial-Twin-Frontend/src/app/room/room-item/room-item.component.spec.ts
@@ -1,16 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonModule } from '@angular/common';
 import { RoomItemComponent } from './room-item.component';
+import { RoomService } from '../room.service';
+import { of } from 'rxjs';
 
 describe('RoomItemComponent', () => {
   let component: RoomItemComponent;
   let fixture: ComponentFixture<RoomItemComponent>;
+  let mockRoomService: Partial<RoomService>;
 
   beforeEach(async () => {
+    // Create the mock using Jest
+    mockRoomService = {
+      getRooms: jest.fn().mockReturnValue(of([])),
+      createEmptyRoom: jest.fn().mockReturnValue({}) // Make sure to mock this method if it's used in the component
+    };
+
     await TestBed.configureTestingModule({
-      imports: [RoomItemComponent]
+      declarations: [RoomItemComponent],
+      imports: [CommonModule],
+      providers: [
+        { provide: RoomService, useValue: mockRoomService }
+      ]
     })
-        .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(RoomItemComponent);
     component = fixture.componentInstance;
